589c9f418eb84985e41aeb04009aea0ba66776a3
David House <Dahous1@umbc.edu>
2015-09-29 12:02:17 -0400

    Chapter 15

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by David on 9/28/2015.
- */
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/ResetRandomDBH.java
+++ src/ResetRandomDBH.java
@@ -25,15 +25,14 @@ public class ResetRandomDBH extends Application{
     @Override
     public void start(Stage primaryStage) {
         resetBT.setOnAction((ActionEvent e) -> {
-            //showBacks(primaryStage);
             randomCards(primaryStage);
         });
-        randomCards(primaryStage);
+        //randomCards(primaryStage);
     }
 
     public void randomCards(Stage primaryStage) {
         Integer[] list = new Integer[52];
-        for (int i = 0; i < 54; i++)
+        for (int i = 0; i < 52; i++)
             list[i] = i + 1;
 
         ArrayList<Integer> deck = new ArrayList<>(Arrays.asList(list));
@@ -62,26 +61,5 @@ public class ResetRandomDBH extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    /*public void showBacks(Stage primaryStage) {
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.BASELINE_LEFT);
-        gridPane.setPadding(new Insets(25, 25, 25, 25));
-        gridPane.setHgap(35);
-        gridPane.setVgap(5);
-
-        ImageView viewCard1 = new ImageView("card/b1fv.png");
-        ImageView viewCard2 = new ImageView("card/b2fv.png");
-
-        gridPane.add(viewCard1, 0, 0);
-        gridPane.add(viewCard2, 1, 0);
-        gridPane.add(viewCard1, 2, 0);
-        gridPane.add(viewCard2, 3, 0);
-        gridPane.add(viewCard1, 4, 0);
-        gridPane.add(resetBT, 2, 1);
-
-        Scene scene = new Scene(gridPane, 600, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }*/
 
 }
